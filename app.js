ea0b56c269b748b1f610404b638fdf2709190d63
mubashirshayan <121729482+mubashirshayan@users.noreply.github.com>
2023-04-19 20:25:40 +0500

    New files

---
+++ app.js
@@ -0,0 +1,21 @@
+//JavaScript assignment no2 Chapter no3
+//Question no1
+//var age='I am 19 year old'
+//Question no2
+/*var visit;
+visit=12
+alert('You have visited this site'+visit+'times')*)
+
+//Question no3
+/*var birth='My Birth year is'
+var birthAre=1990
+var type=typeof(birthAre)
+document.write(birth);
+document.write(birthAre+'<br>')
+document.write('Data type of my declared variable is'+type)*/
+
+//Question no4
+/*var visitorName='Shayan raees'
+var product='T-shirt'
+var quantity=5
+document.write(visitorName+' '+'ordered'+' '+quantity+product+'(s) on XYZ clothing store')*/
